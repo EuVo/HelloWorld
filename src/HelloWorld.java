7c6e483735c3c754df237a9a2f689059f92bf6fe
EuVo <zhekazhvolgin@gmail.com>
2021-10-04 22:40:37 +0300

    Second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     public static void main(String []args) {
         System.out.println("HI!");
         System.out.println("World!");
-
+        System.out.println("Yes");
     }
 }
