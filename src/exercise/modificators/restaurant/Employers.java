b26e9f18ce25e1c36933ea40f635aafab85c0f3b
EuVo <zhekazhvolgin@gmail.com>
2021-11-02 20:45:22 +0300

    present restaurant structure

---
+++ src/exercise/modificators/restaurant/Employers.java
@@ -0,0 +1,23 @@
+package exercise.modificators.restaurant;
+
+public class Employers extends Restaurant{
+    @Override
+    public void orders() {
+
+    }
+
+    @Override
+    public void places() {
+
+    }
+
+    @Override
+    public void payments() {
+
+    }
+
+    //кухня доступна только сотрудникам
+    private void kitchen(){
+
+    }
+}
---
+++ src/exercise/modificators/restaurant/UsualVisitors.java
@@ -0,0 +1,18 @@
+package exercise.modificators.restaurant;
+
+public class UsualVisitors extends Restaurant{
+    @Override
+    public void orders() {
+
+    }
+
+    @Override
+    public void places() {
+
+    }
+
+    @Override
+    public void payments() {
+
+    }
+}
